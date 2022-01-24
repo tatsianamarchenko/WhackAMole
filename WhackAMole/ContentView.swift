46d8823ca9e8a2e6ed9ec8199f9d5e0f744dee3c
tatsiana <tanya3308225@mail.ru>
2022-01-24 11:33:03 +0300

    Initial Commit

---
+++ WhackAMole/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhackAMole
+//
+//  Created by Tatsiana Marchanka on 24.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhackAMole/WhackAMoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhackAMoleApp.swift
+//  WhackAMole
+//
+//  Created by Tatsiana Marchanka on 24.01.22.
+//
+
+import SwiftUI
+
+@main
+struct WhackAMoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
